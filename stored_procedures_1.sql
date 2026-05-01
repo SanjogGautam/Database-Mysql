579fd689e5bf7905d02574407aa2a45ff23aed0d
SanjogGautam <164294030+SanjogGautam@users.noreply.github.com>
2026-05-01 13:05:43 +0545

    sanjog

--- stored_procedures_1.sql
+++ stored_procedures_1.sql
@@ -1,4 +1,13 @@
 /*
 Stored Procedures:
-it is a prepared sql coce that we can save 
+it is a prepared sql code that we can save and can call again and again if we ever need it 
 */
+select * from employees;
+DELIMITER $$
+create procedure get_employees(in f_name varchar(25),in l_name varchar(25))
+begin
+select * from employees
+where first_name=f_name and last_name=l_name;
+end $$
+DELIMITER ;
+call get_employees("Spongebob","Squarepants");
